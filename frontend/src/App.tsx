6f1d3caed429aa5afda420088f49afc90eb85def
Tobias Weber <toby-weber@gmx.de>
2024-11-15 11:58:07 +0100

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,49 @@
+
+import {Todo} from "./Todo.ts";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {allPossibleTodos} from "./TodoStatus.ts";
+import TodoColumn from "./components/TodoColumn.tsx";
+
+
+
+export default function App() {
+
+    const [todos, setTodos] = useState<Todo[]>()
+
+    function fetchTodos(){
+
+        axios.get("api/todo")
+            .then(response => {
+                setTodos(response.data)
+            })
+
+    }
+
+    useEffect(() => fetchTodos, []);
+
+    if (!todos) {
+        return "Lade..."
+    }
+
+    return (
+        <>
+            <div className="page">
+                <h1>TODOs</h1>
+
+                {
+                    allPossibleTodos.map(status => {
+                        const filteredTodos = todos.filter(todo => todo.status === status)
+                        return <TodoColumn status={status}
+                                           todos={filteredTodos}
+                                           onTodoItemChange={fetchTodos}
+                                           key={status}
+                        />
+                    })
+                }
+            </div>
+
+        </>
+    );
+}
+
